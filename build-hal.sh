315f9592b37918ab77d523de1aba74ce1d87e820
Darkness Hidden(TEA) <kmd36rk@gmail.com>
2021-12-18 20:25:50 +0700

    Create build-hal.sh

---
+++ build-hal.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source halium.env
+cd $ANDROID_ROOT
+
+source build/envsetup.sh
+export USE_CCACHE=1
+breakfast $DEVICE
+mka mkbootimg
+mka hybris-boot
+mka halium-boot
+mka recoveryimage
+mka systemimage 
+
+echo "md5sum halium-boot.img and system.img"
+md5sum $ANDROID_ROOT/out/target/product/lantoni/halium-boot.img
+md5sum $ANDROID_ROOT/out/target/product/lantoni/recovery.img
+md5sum $ANDROID_ROOT/out/target/product/lantoni/system.img
